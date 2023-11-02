a75e7cded43235d07319be76ae1efcdc2b081b81
utkarshraj4343 <utkarshraj4343@gmail.com>
2023-11-02 21:47:06 +0530

    added repo link

--- src/Data.js
+++ src/Data.js
@@ -3,31 +3,31 @@ const data = [
     srNo: 1,
     name: "Responsive Paragraph Word Counter",
     depLink: "/wrdcntr",
-    repoLink: "",
+    repoLink: "https://github.com/utkarshraj4343/mern-react-task/tree/main/src/Pages/WordCount",
   },
   {
     srNo: 2,
     name: "Calculator",
     depLink: "/calculator",
-    repoLink: "",
+    repoLink: "https://github.com/utkarshraj4343/mern-react-task/tree/main/src/Pages/Calculator",
   },
   {
     srNo: 3,
     name: "Color Picker",
     depLink: "/clrpicker",
-    repoLink: "",
+    repoLink: "https://github.com/utkarshraj4343/mern-react-task/tree/main/src/Pages/ColorPicker",
   },
   {
     srNo: 4,
     name: "Dummy Data",
     depLink: "/dummydata",
-    repoLink: "",
+    repoLink: "https://github.com/utkarshraj4343/mern-react-task/tree/main/src/Pages/DummyData",
   },
   {
     srNo: 5,
     name: "Age Calculator",
     depLink: "/agecalc",
-    repoLink: "",
+    repoLink: "https://github.com/utkarshraj4343/mern-react-task/tree/main/src/Pages/AgeCalculator",
   }
 ]
 
